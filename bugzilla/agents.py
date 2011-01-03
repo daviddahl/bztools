796a2e6359905a57c8dd329fa03c31cca59a0cc5
David Dahl <david@ddahl.com>
2011-01-03 16:43:33 -0600

    updated agents.py to handle os.environ configuration

--- bugzilla/agents.py
+++ bugzilla/agents.py
@@ -3,8 +3,18 @@ from bugzilla.utils import *
 
 class BugzillaAgent(object):
     def __init__(self, api_root, username=None, password=None):
+        if api_root is None:
+            try:
+                api_root = os.environ['API_ROOT']
+            except:
+                raise "You must provide an API_ROOT ENV VAR or provide one to this constructor"
         self.API_ROOT = api_root
-        self.username, self.password = username, password
+
+        if username is None and password is None:
+            self.username = os.environ.get('BZ_USERNAME', None)
+            self.password = os.environ.get('BZ_PASSWORD', None)
+        else:
+            self.username, self.password = username, password
 
     def get_bug(self, bug, include_fields='_default', exclude_fields=None, params={}):
         params['include_fields'] = include_fields
